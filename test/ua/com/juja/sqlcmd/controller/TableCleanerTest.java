7ed26d5a989e855edffbf55454b8ea4face9dd3d
yarrtuur <sashagmaster@gmail.com>
2018-04-24 16:46:40 +0300

    jUnit 13 TableCleanerTest

--- test/ua/com/juja/sqlcmd/controller/TableCleanerTest.java
+++ test/ua/com/juja/sqlcmd/controller/TableCleanerTest.java
@@ -8,8 +8,8 @@ import ua.com.juja.sqlcmd.types_enums.CmdLineState;
 
 import static org.junit.Assert.*;
 
-public class TableDroperTest {
-    private TableDroper command ;
+public class TableCleanerTest {
+    private TableCleaner command ;
     private DBCommandManager dbManager;
     private DataSet dataSet;
     private String singleCommand;
@@ -17,28 +17,32 @@ public class TableDroperTest {
 
     @Before
     public void setUp() throws Exception {
-        command = new TableDroper ();
+        command = new TableCleaner ();
         dbManager = new DBCommandManager ();
         dbManager.toConnect ( "jdbc:postgresql://127.0.0.1:5432/postgres", "postgres", "1" );
         dataSet = new DataSet ();
         dataSet.add ( "fld", "integer" );
         dbManager.toCreate ( "clone", dataSet );
-        singleCommand = "drop | clone ";
+        dataSet = new DataSet ();
+        dataSet.add ( "fld", "1" );
+        dbManager.toInsert ( "clone", dataSet );
+        singleCommand = "clear | clone ";
         commandLine = singleCommand.replaceAll("\\s", "").toLowerCase().split("\\|");
-        System.out.println ("Start of TableDroperTest");
+        System.out.println ("Start of TableCleanerTest");
     }
 
     @After
     public void tearDown() throws Exception {
+        dbManager.toDrop ( "clone" );
         dbManager.toExit ();
         dbManager = null;
         command = null;
-        System.out.println ("End of TableDroperTest");
+        System.out.println ("End of TableCleanerTest");
     }
 
     @Test
     public void canProcess() {
-        boolean canProcess = command.canProcess ( "drop" );
+        boolean canProcess = command.canProcess ( "clear" );
         assertTrue ( canProcess );
     }
 
@@ -49,7 +53,7 @@ public class TableDroperTest {
 
     @Test
     public void processWrongTableName() {
-        singleCommand = "drop |  ";
+        singleCommand = "clear |  ";
         commandLine = singleCommand.replaceAll("\\s", "").toLowerCase().split("\\|");
         assertEquals ( CmdLineState.WAIT, command.process ( dbManager, commandLine ) );
     }
