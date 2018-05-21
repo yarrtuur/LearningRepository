142db2d6822d0e6426453dc63b57708fd076ec66
yarrtuur <sashagmaster@gmail.com>
2018-05-21 09:27:21 +0300

    HelpTest jUnit

---
+++ src/test/java/ua/com/juja/yar_tur/sqlcmd/controller/commands/HelpTest.java
@@ -0,0 +1,51 @@
+package ua.com.juja.yar_tur.sqlcmd.controller.commands;
+
+import org.junit.*;
+import ua.com.juja.yar_tur.sqlcmd.types_enums_except.CmdLineState;
+import ua.com.juja.yar_tur.sqlcmd.viewer.Console;
+import ua.com.juja.yar_tur.sqlcmd.viewer.View;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelpTest {
+
+	private String singleCommand;
+	private String[] commandLine;
+	private View view;
+	private Help command;
+
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before HelpTest.class");
+	}
+
+	@AfterClass
+	public  static void afterClass() {
+		System.out.println("After HelpTest.class");
+	}
+
+	@Before
+	public void setUp() throws Exception{
+		singleCommand = "help";
+		commandLine = new String[]{"help"};
+		view = new Console();
+		command = new Help(view);
+	}
+
+	@After
+	public void afterTest() {
+		singleCommand = null;
+		view = null;
+	}
+
+	@Test
+	public void canProcessTest(){
+		assertEquals(true, command.canProcess(singleCommand));
+	}
+
+	@Test
+	public void processTest(){
+		assertEquals(CmdLineState.WAIT,command.process(commandLine));
+	}
+
+}
