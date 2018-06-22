09da9dc5dcdc12ff57cc72f7c72c20e1cfc10196
yarrtuur <sashagmaster@gmail.com>
2018-06-22 22:26:40 +0300

    refactor all Commands part 2

--- src/main/java/ua/com/juja/yar_tur/sqlcmd/controller/commands/DataUpdater.java
+++ src/main/java/ua/com/juja/yar_tur/sqlcmd/controller/commands/DataUpdater.java
@@ -11,6 +11,9 @@ import ua.com.juja.yar_tur.sqlcmd.types_enums_except.PrepareResult;
 import ua.com.juja.yar_tur.sqlcmd.viewer.View;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DataUpdater implements CommandProcess, PrepareCmdLine {
     private DBCommandManager dbManager;
@@ -58,26 +61,19 @@ public class DataUpdater implements CommandProcess, PrepareCmdLine {
 
     private void chkAndGetFieldsParams(String[] commandLine) throws ExitException {
         //todo make collection instead array
-        if (commandLine.length % 2 != 0 && commandLine.length > 2) {
+        List<String> cmdList = new ArrayList<String> (Arrays.asList(commandLine));
+        if (cmdList.size() % 2 != 0 && cmdList.size() > 2) {
             throw new ExitException("String format is wrong. Must be even count of data. Try again.");
         } else {
-            int where = 0, set = 0;
-            for (int i = 0; i < commandLine.length; i++) {
-                if (commandLine[i].toLowerCase().equals("set")) {
-                    set = i;
-                }
-                if (commandLine[i].toLowerCase().equals("where")) {
-                    where = i;
-                    break;
-                }
-            }
+            int set = cmdList.indexOf("set");
+            int where = cmdList.indexOf("where");
             dataSetSet = new DataSet();
             for (int i = set + 1; i < where; i += 2) {
-                dataSetSet.add(commandLine[i], commandLine[i + 1]);
+                dataSetSet.add(cmdList.get(i), cmdList.get(i + 1));
             }
             dataSetWhere = new DataSet();
             for (int i = where + 1; i < commandLine.length; i += 2) {
-                dataSetWhere.add(commandLine[i], commandLine[i + 1]);
+                dataSetWhere.add(cmdList.get(i), cmdList.get(i));
             }
         }
     }
