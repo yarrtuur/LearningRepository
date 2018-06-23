ec964c50bd5749de0d88752f58ed22a4e37efb07
yarrtuur <sashagmaster@gmail.com>
2018-06-23 11:11:37 +0300

    refactor deleter & Inserter

--- src/main/java/ua/com/juja/yar_tur/sqlcmd/controller/commands/DataDeleter.java
+++ src/main/java/ua/com/juja/yar_tur/sqlcmd/controller/commands/DataDeleter.java
@@ -5,8 +5,7 @@ import ua.com.juja.yar_tur.sqlcmd.model.DBCommandManager;
 import ua.com.juja.yar_tur.sqlcmd.model.DataSet;
 import ua.com.juja.yar_tur.sqlcmd.model.PrepareCmdLine;
 import ua.com.juja.yar_tur.sqlcmd.types_enums_except.CmdLineState;
-import ua.com.juja.yar_tur.sqlcmd.types_enums_except.FeedBack;
-import ua.com.juja.yar_tur.sqlcmd.types_enums_except.PrepareResult;
+import ua.com.juja.yar_tur.sqlcmd.types_enums_except.ExitException;
 import ua.com.juja.yar_tur.sqlcmd.viewer.View;
 
 import java.sql.SQLException;
@@ -29,42 +28,37 @@ public class DataDeleter implements CommandProcess, PrepareCmdLine {
 
     @Override
     public CmdLineState process(String[] commandLine) {
-        FeedBack resultCode = FeedBack.REFUSE;
-        if (prepareCmdData(commandLine).equals(PrepareResult.PREPARE_RESULT_OK)) {
-            try {
-                view.write("Deleting data from table.");
-                resultCode = dbManager.toDelete(this.tableName, this.dataSet);
-                dbManager.closePrepareStatement();
-            } catch (SQLException ex) {
-                view.write("Delete data interrupted.");
-                view.write(ex.getMessage());
-            }
-        }
-        if( resultCode.equals(FeedBack.OK) ) {
-            view.write("Delete data operation successfull.");
-        } else {
-            view.write("Something wrong with Delete data ...");
+        try {
+            prepareCmdData(commandLine);
+            dbManager.toDelete(this.tableName, this.dataSet);
+        } catch (SQLException | ExitException ex) {
+            view.write(ex.getMessage());
         }
         return CmdLineState.WAIT;
     }
 
     @Override
-    public PrepareResult prepareCmdData(String[] commandLine) {
-        if(commandLine.length > 1){
-            tableName = commandLine[1];
-        } else {
-            view.write("There isn`t tablename at string. Try again.");
-            return PrepareResult.PREPARE_RESULT_WRONG;
-        }
+    public void prepareCmdData(String[] commandLine) throws ExitException {
+        chkAndGetTableName(commandLine);
+        chkAndGetFieldsParams(commandLine);
+    }
+
+    private void chkAndGetFieldsParams(String[] commandLine) throws ExitException {
         if (commandLine.length % 2 != 0 && commandLine.length > 2) {
-            view.write("String format is wrong. Must be even count of data. Try again.");
-            return PrepareResult.PREPARE_RESULT_WRONG;
+            throw new ExitException("String format is wrong. Must be even count of data. Try again.");
         } else {
             dataSet = new DataSet();
             for (int i = 2; i < commandLine.length; i += 2) {
                 dataSet.add(commandLine[i], commandLine[i + 1]);
             }
         }
-        return PrepareResult.PREPARE_RESULT_OK;
+    }
+
+    private void chkAndGetTableName(String[] commandLine) throws ExitException {
+        if (commandLine.length > 1) {
+            tableName = commandLine[1];
+        } else {
+            throw new ExitException("There isn`t tablename at string. Try again.");
+        }
     }
 }
